61dc1aadb576fb7c552d87ba00d6a26a393d89ab
alion02 <alojczek@gmail.com>
2018-11-26 11:25:44 +0100

    Add Fisher-Yates shuffle

---
+++ RLBotAutoRunner/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RLBotAutoRunner
+{
+    public static class Extensions
+    {
+        public static void Shuffle<T>(this Random rand, T[] array)
+        {
+            int i = array.Length;
+            while (i > 1)
+            {
+                int j = rand.Next(i--);
+                T temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+    }
+}
